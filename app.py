f957ad78f5042c362fa3241fb579c26c29d44064
Leo <leo0.moreirah@gmail.com>
2022-10-21 19:54:56 -0300

    Primeira tentativa de publicacao

--- app.py
+++ app.py
@@ -89,5 +89,6 @@ def rsa_descriptografar() -> 'html':
 
 # ========================================================================================
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
