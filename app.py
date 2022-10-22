874b165b7cc9ab652b67bfd17d0c19be9c4fb58a
Leo <leo0.moreirah@gmail.com>
2022-10-22 00:14:09 -0300

    rename app.py

--- app.py
+++ app.py
@@ -91,5 +91,4 @@ def rsa_descriptografar() -> 'html':
 # ========================================================================================
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
